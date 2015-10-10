70167cfd75dfc6b8b9103e8ad7ef93905c638ac0
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-10-10 09:26:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define N 10
+#define N 4
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,33 +8,48 @@ int main(int argc, char** argv) {
     int i = 0;
     float tempoMedio = 0;
     int vet[N]; //Vettore contenente tempi di esecuzione
-    int ris[N]; //Vettore contenente tempi di attesa per ogni processo.
+    float accumulatore; //Vettore contenente tempi di attesa perogni processo.
+    int scelta;
    
 
     // Riempo il vettore con i tempi di esecuzione
+    printf("Inserisci %d processi\n",N);
     while(i < N){
         printf("Inserisci il tempo di esecuzione del %d° processo: ",i + 1);
         scanf("%d",&vet[i]);
         i++;
     }
-    
-    i = 1;
+   
     
     //Calcolo il tempo di attesa di ogni processo e lo sposto in un altro vettore.
-    while(i < N){
-            tempoMedio = tempoMedio + vet[i-1]; 
-            ris[i-1]=tempoMedio;   
-        i++;
+    printf("Quale algoritmo di scheduling vuoi utilizzare ? \n1) FCFS diretto\n2) FCFS inverso\n");
+    scanf("%d",&scelta);
+    switch(scelta){
+        case 1:
+            i = 1;
+            while(i < N){
+                tempoMedio = tempoMedio + vet[i-1]; 
+                accumulatore = accumulatore + tempoMedio;   
+                i++;
+            }
+     
+        break;
+        
+        case 2:
+            i = N - 2;
+            while(i + 1){
+                tempoMedio = tempoMedio + vet[i+1]; 
+                accumulatore = accumulatore + tempoMedio;   
+                i--;            
+            }
+        
+        break;
+        
+        default: printf("%d: scelta non valida.",scelta);
+            
     }
-    
-    //Sommo i tempi di attesa per fare la media.
-    i = 0;
-    tempoMedio = 0;
-    while(i < N - 1){
-        tempoMedio = tempoMedio + ris[i];
-        i++;
-    }
-    tempoMedio = tempoMedio / N;
+   
+    tempoMedio = accumulatore / N;
     printf("Il tempo medio di attesa e' : %f",tempoMedio);
     
     
